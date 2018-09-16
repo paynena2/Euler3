15285ca870262a083a7fd48a96fd99b345c3f544
Allen Payne <Allen Payne@DESKTOP-88ABTDK>
2018-09-16 16:36:18 -0400

    Loop works, trying to figure out why the ArrayList throws an exception when the loop changes values.

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class LargestPrime {
 	public static void main(String[] args) {
-		long init = 600851475143L;
-		
+		long init = 600851475143L; //Given number
+		long stop = init/7; // first number that divides evenly
+		ArrayList<Long> factors = new ArrayList<Long>(); // list of factors
+		for(long x = stop ; x >1; x-- ) {
+			System.out.println("first loop");
+			if(init % x == 0) { // first number mod x should be zero if factor
+				if(isPrime(x)) { //checks if the number is a prime
+					factors.add(x); //if the number is a prime factor, add it to the factor arraylist
+				}
+			}
+		}
+		System.out.println(Collections.max(factors)); // show the largest factor
+	}
+	public static boolean isPrime(long num) {
+		for(long x=2; x < num; x++) {
+			System.out.println("Second loop" + x);
+			if (num % x == 0) {
+				return false;
+			}
+			
+		}
+		return true;
 	}
 }
